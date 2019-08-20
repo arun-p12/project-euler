71f8dad877470cc2a40e0840fc479150ab25ad26
arun-p12 <paramadhathil@gmail.com>
2019-08-20 05:43:34 -0700

    self powers

---
+++ p0001_p0050/p0048.py
@@ -0,0 +1,26 @@
+'''
+Find the least-N significant digits of a series of numbers, raised to itself.
+test :: 1**1 + 2**2 + 3**3 + ... + 10**10 = 10405071317
+'''
+
+# use modulo division to get the least significant digits.
+def self_powers(num, digits=10):
+    last_dig = 0
+    for i in range(1, num+1):
+        last_dig = ((last_dig + (i**i) % (10**digits)) % (10**digits))
+    print("last digits = ", last_dig)
+
+    # sum([i**i for i in range(1,1001)]) % 10000000000      # sngle liner
+
+
+import time      # get a sense of the time taken
+t = time.time()  # get time just before the main routine
+
+########## the main routine #############
+
+self_powers(1000)
+
+########## end of main routine ###########
+
+t = time.time() - t  # and now, after the routine
+print("time       = {:7.5f} s\t{:7.5f} ms\t{:7.3f} µs".format(t, t * 1000, t * 100000))
