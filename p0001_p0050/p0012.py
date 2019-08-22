2d8c3b17d19d7139394ce3fe0e3629cdd0454b30
arun-p12 <paramadhathil@gmail.com>
2019-08-22 13:58:54 -0700

    reduced from 7s to 100ms using memoization

--- p0001_p0050/p0012.py
+++ p0001_p0050/p0012.py
@@ -8,12 +8,26 @@ def highly_divisible_tri_number(number=500):
     sys.path.append('../')
     import common as c
 
-    sum, i, len_fact, factors = 0, 0, 0, []
+    n, len_fact, memo = 1, 0, {}
     while(len_fact < number):
-        i   += 1
-        sum += i
-        factors = c.get_factors(sum)
-        len_fact = len(factors)
+        p, q = n, n + 1
+        if(n % 2 == 0): p = int(p/2)
+        else: q = int(q/2)
+
+        if p in memo: factors1 = memo[p]
+        else:
+            factors1 = c.get_factors(p)
+            memo[p] = factors1
+
+        if q in memo: factors2 = memo[q]
+        else:
+            factors2 = c.get_factors(q)
+            memo[q] = factors2
+
+        len_fact = len(factors1) * len(factors2)
+        n += 1
+    #print("hdtn = ", int(n*(n+1)/2), "[[ ", number, len_fact, " ]]")
+    sum = int(n*(n-1)/2)        # account for +1 above
     print("Triangle number with more than {} factors = {}   [ factors = {} ]".format(number,
                                                                                      sum,
                                                                                      len_fact))
