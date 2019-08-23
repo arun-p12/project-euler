e75d111b0fe8f4a736cb508ffdfb66d3f1d4bc17
arun-p12 <paramadhathil@gmail.com>
2019-08-22 19:00:50 -0700

    even fibonacci numbers

--- p0001_p0050/p0002.py
+++ p0001_p0050/p0002.py
@@ -29,6 +29,6 @@ def p0002_even_fibonacci(**kwargs):
 
     t = time.time() - t             # and now, after the routine
     print("Sum of even Fibonacci upto {} = {}".format(stop, sum))
-    print("time       = {:7.5f} s\t{:7.5f} ms\t{:7.3f} µs".format(t, t*1000, t*100000))
+    print("time       = {:7.5f} s\t{:7.5f} ms\t{:7.3f} µs".format(t, t*1000, t*1_000_000))
 
 p0002_even_fibonacci(verbose=1)
