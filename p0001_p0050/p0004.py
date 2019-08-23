cdcd5e500e1d65f6344d5b4cfafefe8b1c24cd95
arun-p12 <paramadhathil@gmail.com>
2019-08-22 19:00:50 -0700

    largest palindrome product

--- p0001_p0050/p0004.py
+++ p0001_p0050/p0004.py
@@ -20,4 +20,4 @@ for i in range(999, 100, -1):
 
 t = time.time() - t             # and now, after the routine
 print("Required palindrome is {} :: product of {} and {}".format(maxp, ii, jj))
-print("time       = {:7.5f} s\t{:7.5f} ms\t{:7.3f} µs".format(t, t*1000, t*100000))
+print("time       = {:7.5f} s\t{:7.5f} ms\t{:7.3f} µs".format(t, t*1000, t*1_000_000))
