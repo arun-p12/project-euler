8017d74685e2e159d4b0634d4618088e456e6347
arun-p12 <paramadhathil@gmail.com>
2019-08-22 19:06:38 -0700

    amicable numbers

--- p0001_p0050/p0021.py
+++ p0001_p0050/p0021.py
@@ -37,4 +37,4 @@ amicable = amicable_numbers(number)
 
 t = time.time() - t  # and now, after the routine
 print("Sum of all amicable numbers under {} = {}".format(number, amicable))
-print("time       = {:7.5f} s\t{:7.5f} ms\t{:7.3f} µs".format(t, t * 1000, t * 100000))
+print("time       = {:7.5f} s\t{:7.5f} ms\t{:7.3f} µs".format(t, t * 1000, t*1_000_000))
