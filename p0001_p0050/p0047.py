30f4d5566ff5bded37817b4224e8871711f5643a
arun-p12 <paramadhathil@gmail.com>
2019-08-22 19:12:34 -0700

    distinct prime factors

--- p0001_p0050/p0047.py
+++ p0001_p0050/p0047.py
@@ -33,5 +33,5 @@ distinct_prime_factors(4, 4)
 ########## end of main routine ###########
 
 t = time.time() - t  # and now, after the routine
-print("time       = {:7.5f} s\t{:7.5f} ms\t{:7.3f} µs".format(t, t * 1000, t * 100000))
+print("time       = {:7.5f} s\t{:7.5f} ms\t{:7.3f} µs".format(t, t * 1000, t*1_000_000))
 
