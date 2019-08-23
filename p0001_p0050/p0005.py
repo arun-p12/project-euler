e9b377e0b0cc52c544f1bff4f3a54d962e1371b0
arun-p12 <paramadhathil@gmail.com>
2019-08-22 19:00:50 -0700

    smallest multiple

--- p0001_p0050/p0005.py
+++ p0001_p0050/p0005.py
@@ -21,4 +21,4 @@ for i in (range(1, number+1)):          # the full range of numbers to be checke
 
 t = time.time() - t  # and now, after the routine
 print("Least common multiple of all numbers upto {} = {}".format(number, result))
-print("time       = {:7.5f} s\t{:7.5f} ms\t{:7.3f} µs".format(t, t * 1000, t * 100000))
+print("time       = {:7.5f} s\t{:7.5f} ms\t{:7.3f} µs".format(t, t * 1000, t*1_000_000))
